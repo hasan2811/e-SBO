8565a3985a1fa6c9ca3d8b307dab678e15464982
Beriandi Rully <beriandirully@gmail.com>
2025-06-24 23:38:27 +0000

    ok cek lagi app rapikan code lagi buang yang tidak perlu pastian pwa app

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -103,7 +103,7 @@ export default function TasksPage() {
   return (
     <div className="space-y-6">
       <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold tracking-tight">Semua Temuan</h2>
+        <h2 className="text-2xl font-bold tracking-tight">Tugas</h2>
       </div>
 
        {observations.length > 0 ? (
