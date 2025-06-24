a471d742807c4bc1f81e54810d1db11cacd33696
Beriandi Rully <beriandirully@gmail.com>
2025-06-24 21:50:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/layout.tsx
+++
@@ -1,2 +0,0 @@
-// This file is part of an obsolete route group and has been disabled to prevent build conflicts.
-// The active layout is located at /src/app/layout.tsx.
--- src/app/(main)/tasks/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is part of an obsolete route group and has been disabled to prevent build conflicts.
-// The active page is located at /src/app/tasks/page.tsx.
