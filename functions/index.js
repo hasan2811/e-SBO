c1dcd54423dfd9220434e0cd3e9f0acce6feebc6
Beriandi Rully <beriandirully@gmail.com>
2025-06-25 09:21:31 +0000

    pada saat aku upload ada tulisan submision failed

--- functions/index.js
+++ functions/index.js
@@ -1,60 +1,70 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const cors = require("cors")({origin: true});
+const busboy = require("busboy");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
 admin.initializeApp();
 
 const storage = admin.storage();
 
-exports.uploadPhoto = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
-  }
+exports.uploadPhoto = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== "POST") {
+      return res.status(405).send("Method Not Allowed");
+    }
 
-  const busboy = require("busboy").default;
+    const bb = busboy({headers: req.headers});
+    const tmpdir = os.tmpdir();
 
-  const bb = busboy({headers: req.headers});
-  let fileBuffer = Buffer.from("");
-  let fileName = "";
-  let mimeType = "";
+    let uploadData = null;
+    let localFilePath = null;
 
-  bb.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    fileName = filename;
-    mimeType = mimetype;
-    file.on("data", (data) => {
-      fileBuffer = Buffer.concat([fileBuffer, data]);
+    bb.on("file", (fieldname, file, info) => {
+      const {filename, mimeType} = info;
+      const filepath = path.join(tmpdir, filename);
+      localFilePath = filepath;
+      uploadData = {mimeType: mimeType, name: filename};
+      const writeStream = fs.createWriteStream(filepath);
+      file.pipe(writeStream);
     });
-    file.on("end", () => {
-      console.log("File received");
+
+    bb.on("finish", async () => {
+      if (!uploadData || !localFilePath) {
+        return res.status(400).send("No file uploaded.");
+      }
+
+      const bucket = storage.bucket();
+      const destination = `photos/${uploadData.name}`;
+
+      try {
+        const [file] = await bucket.upload(localFilePath, {
+          destination: destination,
+          metadata: {
+            contentType: uploadData.mimeType,
+          },
+        });
+
+        fs.unlinkSync(localFilePath); // Clean up the temporary file
+
+        const [url] = await file.getSignedUrl({
+          action: "read",
+          expires: "03-09-2491",
+        });
+
+        console.log(`File uploaded successfully to ${destination}`);
+        res.status(200).send({url: url});
+      } catch (err) {
+        console.error("Error during upload process:", err);
+        if (localFilePath) {
+          fs.unlinkSync(localFilePath);
+        }
+        res.status(500).send("Error uploading file.");
+      }
     });
+
+    req.pipe(bb);
   });
-
-  bb.on("finish", async () => {
-    const bucket = storage.bucket();
-    // Store in a 'photos' folder
-    const fileUpload = bucket.file(`photos/${fileName}`);
-
-    const stream = fileUpload.createWriteStream({
-      metadata: {
-        contentType: mimeType,
-      },
-    });
-
-    stream.on("error", (err) => {
-      console.error("Error uploading file:", err);
-      res.status(500).send("Error uploading file");
-    });
-
-    stream.on("finish", async () => {
-      console.log("File uploaded successfully");
-      // Optionally, get the download URL
-      const [url] = await fileUpload.getSignedUrl({
-        action: "read",
-        expires: "03-09-2491", // Set an appropriate expiry date
-      });
-      res.status(200).send({url});
-    });
-
-    stream.end(fileBuffer);
-  });
-
-  bb.end(req.rawBody); // Use req.rawBody for the raw request body
 });
