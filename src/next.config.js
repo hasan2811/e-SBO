3db940ad20df5b68499bc9ece8b6029ab14791a4
Beriandi Rully <beriandirully@gmail.com>
2025-06-26 14:45:47 +0000

    logo n di kiri bawah masih terlihat

---
+++ src/next.config.js
@@ -0,0 +1,53 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: false,
+});
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+   webpack: (
+    config,
+    { isServer }
+  ) => {
+    // Aliasing to fix client-side bundling issues with server-only packages
+    if (!isServer) {
+      config.resolve.alias['@opentelemetry/exporter-jaeger'] = false;
+      config.resolve.alias['@opentelemetry/sdk-node'] = false;
+    }
+    return config
+  }
+};
+
+module.exports = withPWA(nextConfig);
