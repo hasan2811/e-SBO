76f70fdced8d489d3244bb223e57a28da9598eb8
Beriandi Rully <beriandirully@gmail.com>
2025-06-28 15:10:31 +0000

    ingat perintah saya lupakan masalah aturan kearaman key dan firbase conf

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,2 @@
-
-import * as admin from 'firebase-admin';
-
-// In a Google Cloud environment like App Hosting, the SDK is automatically
-// configured with the project's credentials. We don't need to pass any config.
-if (!admin.apps.length) {
-  admin.initializeApp();
-}
-
-// Export the initialized admin services.
-const storage = admin.storage();
-export { storage };
+// This file has been intentionally left blank.
+// It was causing a build error and is not used by the application.
